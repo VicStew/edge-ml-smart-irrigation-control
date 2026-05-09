678d652afccb64495251021ba76fdf71a6b102d0
VicStew <victorstew5@gmail.com>
2026-05-10 02:24:42 +0300

    project folder cleanup

--- weather_irrigation_amount/et0_model.h
+++
@@ -1,7 +0,0 @@
-#ifndef ET0_MODEL_TFLITE_H
-#define ET0_MODEL_TFLITE_H
-
-extern const unsigned char et0_model_tflite[];
-extern const int et0_model_tflite_len;
-
-#endif // ET0_MODEL_TFLITE_H
--- weather_irrigation_amount/precip_model.h
+++
@@ -1,7 +0,0 @@
-#ifndef PRECIP_MODEL_TFLITE_H
-#define PRECIP_MODEL_TFLITE_H
-
-extern const unsigned char precip_model_tflite[];
-extern const int precip_model_tflite_len;
-
-#endif // PRECIP_MODEL_TFLITE_H
