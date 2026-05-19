36a04dc93e56b15ff72f799c1d82879865173bda
VicStew <victorstew5@gmail.com>
2026-05-19 17:31:46 +0300

    Save initial state of rainfall_convolution before updates

---
+++ rainfall_convolution/cnn_rainfall_forecaster.h
@@ -0,0 +1,7 @@
+#ifndef CNN_RAINFALL_FORECASTER_H
+#define CNN_RAINFALL_FORECASTER_H
+
+extern const unsigned char cnn_rainfall_forecaster_tflite[];
+extern const int cnn_rainfall_forecaster_tflite_len;
+
+#endif  // CNN_RAINFALL_FORECASTER_H
